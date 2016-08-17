1f4b2394c0cf6cbfa9f08c4a01f49dbb990dbc11
cschleiden <cschleiden@live.de>
2016-08-16 22:22:03 -0700

    Fix tests for strategy

--- src/strategies/clone.tests.ts
+++ src/strategies/clone.tests.ts
@@ -17,15 +17,15 @@ class Complex implements IComplex {
 }
 
 describe("default clone strategy", () => {
-    it("fails for complex objects", () => {
-        let strategy = new DefaultCloneStrategy();
-        expect(() => strategy.clone(new Complex())).to.throw();
-    });
-
     it("succeeds for simple objects", () => {
         let strategy = new DefaultCloneStrategy();
         let a = { foo: 42 };
         let b = strategy.clone(a);
         expect(a).to.be.not.equal(b);
+    });
+
+    it("fails for complex objects", () => {
+        let strategy = new DefaultCloneStrategy();
+        expect(() => strategy.clone(new Complex())).to.throw();
     });
 })
